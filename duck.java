dd1105e01b5055ac5aaef1413d3dc7d8e36886e4
Chowdury <chowdury@UMICH.EDU>
2019-10-10 16:57:03 -0400

    asfdaf

---
+++ duck.java
@@ -0,0 +1,15 @@
+Public class duck {
+
+/*
+ * Master branch is responsible for fly() method.
+ */
+public fly(){
+		system.out.println("A duck can fly");
+	}
+
+public eat(){
+		system.out.println("A duck can eat");
+
+	}
+
+}
